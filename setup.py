e72d8bda5714d4172d35f2ac19a6b4ce7a6e6aa3
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2021-12-11 02:29:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     install_requires=[
         "licorice>=0.0.2",
         "pandas>=1.3.4",
-        "BeautifulSoup",
+        "beautifulsoup4>=4.10.0",
     ],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
