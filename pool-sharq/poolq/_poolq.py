960e5ea5a887cf9ca608f474cb48f461bd47c305
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2021-12-11 02:03:09 -0500

    Create _poolq.py

---
+++ pool-sharq/poolq/_poolq.py
@@ -0,0 +1,15 @@
+class _poolq:
+
+  def __init__(self):  
+    self.path = path
+    self.path_to_executable = "/external/" # finish 
+    self.executable = _look_for_poolq(self.path_to_executable) # maybe this logic isn't the best... should fetch be nested in this dir?    
+    
+  def run(self):
+    
+    ...
+    
+    
+  def parse(self):
+    
+    ...
