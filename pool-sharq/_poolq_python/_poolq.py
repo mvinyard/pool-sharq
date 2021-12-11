8a2ffb1ba66e9afac9c642db901ddace94c395e6
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2021-12-11 02:21:48 -0500

    Update _poolq.py

--- pool-sharq/_poolq_python/_poolq.py
+++ pool-sharq/_poolq_python/_poolq.py
@@ -1,15 +1,93 @@
+
+from ._executable import _executable
+
 class _poolq:
 
-  def __init__(self):  
-    self.path = path
-    self.path_to_executable = "/external/" # finish 
-    self.executable = _look_for_poolq(self.path_to_executable) # maybe this logic isn't the best... should fetch be nested in this dir?    
+  def __init__(self,
+        data_dir=False,
+        metadata=False,
+        outpath=False,        
+    ):
+
+      self.data_dir = data_dir
+      barcode_indicator="barcode",
+      barcode_filename="rows.txt",
+      conditions_filename="column.txt",
+      self.standard_PoolQ_outfiles = standard_PoolQ_outfiles=[
+          "barcode-counts.txt",
+          "correlation.txt",
+          "counts.txt",
+          "lognormalized-counts.txt",
+          "poolq3.log",
+          "quality.txt",
+          "runinfo.txt",
+          "unexpected-sequences.txt",
+      ],
+      
+    def run(
+        self,
+        data_dir=False,
+        run_name=False,
+        dry_run=False,
+        barcode_filename=False,
+        conditions_filename=False,
+        barcode_indicator=False,
+        outpath=False,
+    ):
+        if not run_name:
+            self.run_name = "run_{}".format(
+                "".join(np.random.choice(list(string.ascii_lowercase), 6))
+            )
+        else:
+            self.run_name = run_name
+
+        if data_dir:
+            self.data_dir = data_dir
+        if barcode_filename:
+            self.barcode_filename = barcode_filename
+        if conditions_filename:
+            self.conditions_filename = conditions_filename
+        if barcode_indicator:
+            self.barcode_indicator = barcode_indicator
+        if outpath:
+            self.outpath = outpath
+
+        _executable(
+            self.data_dir,
+            self.barcode_filename,
+            self.conditions_filename,
+            poolq_path,
+            self.barcode_indicator,
+            dry_run,
+            self.run_name,
+        )
+
+        if not dry_run:
+            self.outs = _organize_PoolQ_outputs(
+                self.run_name,
+                self.standard_PoolQ_outfiles,
+                self.outpath,
+                fetch_only=False,
+            )
+            self.ResultsDict = _make_results_dict(self.outs)
     
-  def run(self):
     
-    ...
+  def parse(self,
+            run_name=False,
+            standard_PoolQ_outfiles=False,
+            outpath=False,
+            fetch_only=False,):
     
-    
-  def parse(self):
-    
-    ...
+    if run_name:
+            self.run_name = run_name
+
+    if standard_PoolQ_outfiles:
+        self.standard_PoolQ_outfiles = standard_PoolQ_outfiles
+
+    if outpath:
+        self.outpath = outpath
+
+    self.outs = _organize_PoolQ_outputs(
+        self.run_name, self.standard_PoolQ_outfiles, self.outpath, fetch_only
+    )
+    self.ResultsDict = _make_results_dict(self.outs)
