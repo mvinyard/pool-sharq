4fa51a2c4973d2278027931a2e02e4e702ff809d
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2021-12-13 23:15:24 -0500

    Update __init__.py

--- pool_sharq/__init__.py
+++ pool_sharq/__init__.py
@@ -1,4 +1,8 @@
-# __init__.py
+
+__module_name__ = "__init__.py"
+__author__ = ", ".join(["Michael E. Vinyard"])
+__email__ = ", ".join(["vinyard@g.harvard.edu",])
+
 
 from ._fetch._poolq_downloader import _look_for_poolq
 from ._fetch._download_test_data import _download_test_data as download_test_data
