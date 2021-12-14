6c304663dba719efbd603348f69e76393681123a
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2021-12-13 23:16:03 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import sys
 
 setup(
     name="pool-sharq",
-    version="0.0.0",
+    version="0.0.1",
     python_requires=">3.7.0",
     author="Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard",
     author_email="mvinyard@broadinstitute.org",
