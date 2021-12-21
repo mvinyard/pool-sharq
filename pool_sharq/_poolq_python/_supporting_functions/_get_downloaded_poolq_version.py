0cde269506d11ca83176f79fc99c0723340137d2
mvinyard <mvinyard@broadinstitute.org>
2021-12-21 05:57:40 +0000

    fix distribution-get

--- pool_sharq/_poolq_python/_supporting_functions/_get_downloaded_poolq_version.py
+++ pool_sharq/_poolq_python/_supporting_functions/_get_downloaded_poolq_version.py
@@ -16,7 +16,7 @@ def _get_downloaded_poolq_version():
     """"""
     
     package_path  = pyrex.parent_dir(__file__, levels=3)
-    poolq_distribution_dir = glob.glob(os.path.join(package_path, "_distribution/*"))[0] 
+    poolq_distribution_dir = glob.glob(os.path.join(package_path, "_distribution/poolq-*"))[0]
     poolq_version = poolq_distribution_dir.split('-')[-1]
     poolq_path = os.path.join(poolq_distribution_dir, "poolq3.sh")
     
